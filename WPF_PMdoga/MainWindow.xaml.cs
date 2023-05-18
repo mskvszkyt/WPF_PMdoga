2d6c38e78326ea8254694b18d2a51d1a621e37b1
mskvszkyt <muskovszkyt@gmail.com>
2023-05-18 14:46:30 +0200

    Add project files.

---
+++ WPF_PMdoga/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_PMdoga
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Szakasz> szakaszok = new List<Szakasz>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ListaBetoltes();
+            MyDg.ItemsSource = szakaszok;
+        }
+
+        public void ListaBetoltes()
+        {
+            var adatok = File.ReadAllLines("adatok.txt",Encoding.UTF8);
+            foreach (var item in adatok)
+            {
+                Szakasz uj = new Szakasz(item);
+                szakaszok.Add(uj);
+            }
+        }
+
+        private void szakaszok_szama_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"szakaszok száma {szakaszok.Count()}");
+        }
+
+        private void teljesitheto_Click(object sender, RoutedEventArgs e)
+        {
+            var szurt = szakaszok.Select(x => x).Where(x => x.Ido <= Convert.ToInt32(tbido.Text)).ToList();
+            MyDg.ItemsSource = szurt;
+            lbszurtek.Content = szurt.Count;
+        }
+
+        private void pilis_Click(object sender, RoutedEventArgs e)
+        {
+            var eredmeny = szakaszok.Select(x => x).Where(y => y.Utleiras.Contains("Pilis")).Select(z => z.Ido).Average();
+            MessageBox.Show($"pilis szavas szakaszok átlag ideje:{eredmeny}");
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            MyDg.ItemsSource = szakaszok;
+        }
+
+        private void lehosszabb_Click(object sender, RoutedEventArgs e)
+        {
+            var eredmeny = szakaszok.Select(x => x).Where(x => x.Hossz == szakaszok.Max(x => x.Hossz)).ToList();
+            foreach (var item in eredmeny)
+            {
+                MessageBox.Show($"{item.Utleiras},{item.Hossz},{item.Ido}");
+            }
+        }
+
+        private void mentes_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> eredmeny = new List<string>();
+
+            foreach (var item in szakaszok)
+            {
+                eredmeny.Add($"{item.Utleiras},{item.Hossz},{item.Magassag},{item.Nehezseg(item.Hossz)}");
+            }
+            File.WriteAllLines("utvonal.txt",eredmeny,Encoding.UTF8);
+        }
+    }
+}
---
+++ WPF_PMdoga/Szakasz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_PMdoga
+{
+    internal class Szakasz
+    {
+        //Csényeújmajor - Sárvár v.áll.   ;  4,5 ;   0  ;   68
+        string utleiras;
+        int magassag;
+        int ido;
+        double hossz;
+
+        public Szakasz(string txtSor)
+        {
+            var mezok = txtSor.Split(";");
+            utleiras = mezok[0];
+            ido = Convert.ToInt32(mezok[3]);
+            magassag = Convert.ToInt32(mezok[2]);
+            hossz = Convert.ToDouble(mezok[1]);
+        }
+
+        public string Utleiras { get => utleiras; }
+        public int Magassag { get => magassag; }
+        public int Ido { get => ido; }
+        public double Hossz { get => hossz; }
+
+        public string Nehezseg(double hossz)
+        {
+            if (hossz <= 5)
+            {
+                return "könnyű";
+            }
+            else if (hossz < 10)
+            {
+                return "közepes";
+            }
+            else
+            {
+                return "nehéz";
+            }
+        }
+    }
+}
